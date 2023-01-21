64f7c187202798cb60823c464675781b55ce65f2
Yugi Nova <yuginovaniac@gmail.com>
2023-01-21 11:34:50 +0700

    local chagne again 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
 let a = 'hello world';
-<<<<<<< HEAD
 let b = 'local change';
 console.log("hello world \n" + a);
-=======
-let b = 'github change';
-console.log("hello world \n" + a);
->>>>>>> e1592919a884cf4a39f651346c9c1e9151da9a10
